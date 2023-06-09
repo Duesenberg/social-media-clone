f7d199762dcafe6ac8d32efcec46f982f49859a1
Pande <popovski.pande@gmail.com>
2023-06-09 11:34:13 +0200

    set up firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1uQkysF-evbvjH15InyGmHtP98FdJnM4",
+  authDomain: "social-media-clone-a65ab.firebaseapp.com",
+  projectId: "social-media-clone-a65ab",
+  storageBucket: "social-media-clone-a65ab.appspot.com",
+  messagingSenderId: "1006610018179",
+  appId: "1:1006610018179:web:931f9550d169b4588a10b6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
