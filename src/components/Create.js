a6a267ea8d620b4a30bd39eb670d404245a3cd00
Pande <popovski.pande@gmail.com>
2023-06-26 16:01:51 +0200

    create & style create section

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import '../styles/Create.css';
+
 export default function Create () {
   return (
-    <div>Hello from Create</div>
+    <div className='create-container'>
+      <div className='card'>
+        <p className='title'>Create a new post</p>
+
+        <form className='input-container'>
+          <input type='text' name='post-caption' id='postCaption' placeholder="What's on your mind?" />
+
+          <input type="file" id='addPostPhoto' />
+          <label htmlFor='addPostPhoto' className='post-photo-label'>Add Photo to Post</label>
+
+          <button>Submit Post</button>
+        </form>
+      </div>
+    </div>
   )
 }
