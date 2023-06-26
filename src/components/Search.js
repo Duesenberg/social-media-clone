a8aa412a55e4cccf92406f2c598522d9979a0f57
Pande <popovski.pande@gmail.com>
2023-06-26 15:30:20 +0200

    create & style search section

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import '../styles/Search.css';
+import SearchResults from './SearchResults';
+
 export default function Search () {
   return (
-    <div>Hello from Search</div>
+    <div className='search-container'>
+      <div className='sub-container'>
+        <input type='text' name='profile-search' placeholder='Search' />
+        <SearchResults />
+      </div>
+    </div>
   )
 }
---
+++ src/components/SearchResults.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default function SearchResults () {
+  return (
+    <div className='search-results'>
+      <div className='result'>
+        <img 
+          className="profile-picture" 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
+          alt="Profile" />
+        <span className="username">John Doe</span>
+      </div>
+      
+      <div className='result'>
+        <img 
+          className="profile-picture" 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
+          alt="Profile" />
+        <span className="username">John Doe</span>
+      </div>
+
+      <div className='result'>
+        <img 
+          className="profile-picture" 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
+          alt="Profile" />
+        <span className="username">John Doe</span>
+      </div>
+
+      <div className='result'>
+        <img 
+          className="profile-picture" 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
+          alt="Profile" />
+        <span className="username">John Doe</span>
+      </div>
+
+      <div className='result'>
+        <img 
+          className="profile-picture" 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
+          alt="Profile" />
+        <span className="username">John Doe</span>
+      </div>
+    </div>
+  )
+}
