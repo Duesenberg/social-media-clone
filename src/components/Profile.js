9a8836d459e8bb0c92677dc7fedd2ff1bb9c1f4e
Pande <popovski.pande@gmail.com>
2023-06-27 01:29:52 +0200

    create & style profile section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,29 @@
+import React from 'react';
+import Post from './Post';
+import '../styles/Profile.css';
+
 export default function Profile () {
   return (
-    <div>Hello from Profile</div>
+    <div className='profile-container'>
+      <div className='profile-header'>
+        <img 
+          className="profile-picture" 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
+          alt="Profile" />
+
+        <div className='profile-info'>
+          <p className='username'>John Doe</p>
+          <p className='bio'>No hill for a stepper</p>
+          <button className='edit-profile'>Edit Profile</button>
+        </div>
+      </div>
+
+      <p className='posts-title'>Posts</p>
+      <div className='profile-posts'>
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
   )
 }
