775041d11658383056d4e7b650120465b9239fb8
Pande <popovski.pande@gmail.com>
2023-06-29 14:07:11 +0200

    error handling in register page

--- src/Register.js
+++ src/Register.js
@@ -1,30 +1,98 @@
 import { Link } from 'react-router-dom';
 import './styles/RegisterAndLogin.css';
-import { auth } from './firebase';
+import { auth, storage } from './firebase';
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 export default function Register () {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     //Get form data
-    const fullName = e.target[0].value;
+    const userName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
-    //Firebase authentication
+    //Run firebase auth & upload photo to storage
     try {
+      //Firebase authentication
       const res = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(res.user);
+
+      //Firebase storage for photo upload
+      const storageRef = ref(storage, userName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on('state_changed',
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+          }
+        },  
+        (error) => {
+          // Handle unsuccessful uploads
+          console.log(error);
+          const uploadButton = document.querySelector('.file-label');
+          uploadButton.textContent = 'Upload unsuccessful';
+          uploadButton.classList.add('unsuccessful');
+        }, 
+        () => {
+          // Handle successful uploads on complete
+          const uploadButton = document.querySelector('.file-label');
+          uploadButton.textContent = 'Upload successful';
+          uploadButton.classList.remove('unsuccessful');
+          uploadButton.classList.add('successful');
+        }
+      );
+
+      //Handle register successful message
+      const errorMessage = document.querySelector('.error-handling')
+      errorMessage.classList.remove('unsuccessful');
+      errorMessage.classList.add('successful');
+      errorMessage.textContent = 'Register successful';
     } catch(err) {
       console.log(err);
+
+      //Handle error message
+      const errorMessage = document.querySelector('.error-handling')
+      errorMessage.classList.remove('successful');
+      errorMessage.classList.add('unsuccessful');
+
+      switch(err.message) {
+        case 'Firebase: Error (auth/email-already-in-use).':
+          errorMessage.textContent = 'Email already in use.';
+          break;
+        case 'Firebase: Error (auth/invalid-email).':
+          errorMessage.textContent = 'Invalid email address.';
+          break;
+        default: 
+          errorMessage.textContent = 'Register unsuccessful';
+          break;
+      }
     }
   }
 
   const handleClick = (e) => {
     //Add class of submitted to form on click
     e.target.parentElement.classList.add('submitted');
+  }
+
+  //Remove any error messages from upload button when clicked
+  const handlePhotoClick = () => {
+    const uploadButton = document.querySelector('.file-label');
+    uploadButton.classList.remove('unsuccessful');
+    uploadButton.classList.remove('successful');
+    uploadButton.textContent = 'Select Photo';
   }
 
   return (
@@ -46,9 +114,13 @@ export default function Register () {
             id='addFile'
             accept=".jpg, .jpeg" 
             required />
-          <label htmlFor='addFile' className='file-label'>Select Photo</label>
-          <span className='photo-validation'>Please select a photo</span>
+          <label 
+            htmlFor='addFile' 
+            className='file-label'
+            onClick={handlePhotoClick}>Select Photo</label>
+          <span className='photo-validation'>Please select a photo.</span>
           <button onClick={ handleClick }>Register</button>
+          <span className='error-handling'>...</span>
         </form>
 
         <p className='bottom-text'>Already have an account? <span><Link to={'/login'}>Log In</Link></span></p>
