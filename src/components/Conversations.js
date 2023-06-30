cd2aeba3ab8d0010526b80db7ac041d9a622fd97
Pande <popovski.pande@gmail.com>
2023-06-30 21:56:42 +0200

    set up conversations sidebar with backend

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,116 +1,45 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+import { AuthContext } from "../contexts/AuthContext";
 
 export default function Conversations () {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    //Get data from 'userChats' for the current user & set it in chats
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => { 
+        unsub();
+      }
+    }
+    
+    //If there is no current user don't run getChats();
+    currentUser.uid && getChats();
+  }, [currentUser.uid])
+
   return (
     <div className="conversations-container">
-      <button className="conversation">
-        <img 
-          className="profile-picture"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
-          alt="" />
-        
-        <div className="conversation-info">
-          <p className="title">John</p>
-          <p className="last-message">Hello, I'm John Doe</p>
-        </div>
-      </button>
-      
-      <button className="conversation">
-        <img 
-          className="profile-picture"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
-          alt="" />
-        
-        <div className="conversation-info">
-          <p className="title">John</p>
-          <p className="last-message">Hello, I'm John Doe</p>
-        </div>
-      </button>
-      
-      <button className="conversation">
-        <img 
-          className="profile-picture"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
-          alt="" />
-        
-        <div className="conversation-info">
-          <p className="title">John</p>
-          <p className="last-message">Hello, I'm John Doe</p>
-        </div>
-      </button>
-      
-      <button className="conversation">
-        <img 
-          className="profile-picture"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
-          alt="" />
-        
-        <div className="conversation-info">
-          <p className="title">John</p>
-          <p className="last-message">Hello, I'm John Doe</p>
-        </div>
-      </button>
-      
-      <button className="conversation">
-        <img 
-          className="profile-picture"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
-          alt="" />
-        
-        <div className="conversation-info">
-          <p className="title">John</p>
-          <p className="last-message">Hello, I'm John Doe</p>
-        </div>
-      </button>
-      
-      <button className="conversation">
-        <img 
-          className="profile-picture"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
-          alt="" />
-        
-        <div className="conversation-info">
-          <p className="title">John</p>
-          <p className="last-message">Hello, I'm John Doe</p>
-        </div>
-      </button>
-      
-      <button className="conversation">
-        <img 
-          className="profile-picture"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
-          alt="" />
-        
-        <div className="conversation-info">
-          <p className="title">John</p>
-          <p className="last-message">Hello, I'm John Doe</p>
-        </div>
-      </button>
-      
-      <button className="conversation">
-        <img 
-          className="profile-picture"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
-          alt="" />
-        
-        <div className="conversation-info">
-          <p className="title">John</p>
-          <p className="last-message">Hello, I'm John Doe</p>
-        </div>
-      </button>
-      
-      <button className="conversation">
-        <img 
-          className="profile-picture"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutp6Rf6nH24hRGK5NBsW5LFII03lUqcgLNQ&usqp=CAU"
-          alt="" />
-        
-        <div className="conversation-info">
-          <p className="title">John</p>
-          <p className="last-message">Hello, I'm John Doe</p>
-        </div>
-      </button>
-      
+      {Object.entries(chats)?.map((chat) => {
+        return (
+          <button className="conversation" key={chat[0]}>
+            <img 
+              className="profile-picture"
+              src={chat[1].userInfo.photoURL}
+              alt="" />
+            
+            <div className="conversation-info">
+              <p className="title">{chat[1].userInfo.displayName}</p>
+              <p className="last-message">{chat[1].userInfo.lastMessage?.text}</p>
+            </div>
+          </button>
+        )
+      })}
     </div>
   )
 }
