202f7afa65a0807527b2df887136bc90921d7068
Pande <popovski.pande@gmail.com>
2023-07-03 01:46:41 +0200

    rewrite part of handleSubmit in Create.js to avoid some bugs

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 This is a social media clone app made with React and Firebase.
 
+The application is supposed to imitate Instagram. It is developed as a web app,
+an it functions best on a browser. I have also added CSS for a mobile screen,
+however I'm only having an issue with the address bar appearing and disappearing
+when you scroll (it makes the bottom of the screen where there are three buttons
+scroll down, you have to scroll up the address bar so they appear again).
+
+- You can create an account and log in using the credentials. 
+- I haven't made a friend/follow system, so all users that have signed up are
+  available to you
+- You can start chats with the users, and send them text and photos via the chat.
+- You can also make text/photo posts, whic other users can comment on (I haven't
+  implemented likes yet).
+- You can check out your own profile to see what you have posted and sign out 
+  from there (you can't check other people's profiles, I have made placeholder
+  components for this feature but haven't made any functionalities for it yet).
+- I was also planning to make a notification system, however I just made some 
+  placeholder content for it and havent made it functional yet.
+
+Known bugs that I haven't been able to fix:
+- Can't post photo posts or send photos in chat when you're on mobile. The photo
+  gets uploaded in storage, but for some reason the message/post info doesn't 
+  get saved in the database
+- If you want to send a single photo two times in a row, it won't get sent the
+  second time you try it.
+- On occasion, if you send a photo via chat it gets sent two times in a row.
+
 Assets used:
 
 Fonts:
--- src/components/Create.js
+++ src/components/Create.js
@@ -8,8 +8,11 @@ import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
 export default function Create () {
   const { currentUser } = useContext(AuthContext);
-  const [caption, setCaption] = useState('');
+  const [caption1, setCaption1] = useState('');
+  const [caption2, setCaption2] = useState('');
   const [img, setImg] = useState(null);
+
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,31 +25,47 @@ export default function Create () {
 
         const uploadTask = uploadBytesResumable(storageRef, img);
 
-        uploadTask.on(
-          (error) => {
-            //TODO:Handle Error
-          },
-          () => {
-            getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-              await addDoc(collection(db, 'posts'), {
-                uid: currentUser.uid,
-                displayName: currentUser.displayName,
-                photoURL: currentUser.photoURL,
-                caption: caption,
-                date: Timestamp.now(),
-                comments: [],
-                img: downloadURL
-              })
-            });
+        uploadTask.on('state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          // console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              // console.log('Upload is paused');
+              break;
+            case 'running':
+              // console.log('Upload is running');
+              break;
           }
-        );
+        },  
+        (error) => {
+          // Handle unsuccessful uploads
+          // console.log(error);
+        }, 
+        () => {
+          // Handle successful uploads on complete
+
+          //Update user data
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await addDoc(collection(db, 'posts'), {
+              uid: currentUser.uid,
+              displayName: currentUser.displayName,
+              photoURL: currentUser.photoURL,
+              caption: caption1,
+              date: Timestamp.now(),
+              comments: [],
+              img: downloadURL
+            })
+          });
+        }
+      );
       } else {
         //If there is no photo uploaded, create a doc without img url:
         await addDoc(collection(db, 'posts'), {
           uid: currentUser.uid,
           displayName: currentUser.displayName,
           photoURL: currentUser.photoURL,
-          caption: caption,
+          caption: caption2,
           date: Timestamp.now(),
           comments: []
         })
@@ -55,14 +74,15 @@ export default function Create () {
       console.log(err);
     }
 
-    setCaption('');
+    setCaption1('');
+    setCaption2('');
     setImg(null);
   }
   
   return (
     <div className='create-container'>
       <div className='card'>
-        <p className='title'>Create a new post</p>
+        <p className='title'>Create a new text post</p>
 
         <form onSubmit={handleSubmit} className='input-container'>
           <input 
@@ -70,15 +90,36 @@ export default function Create () {
             name='post-caption' 
             id='postCaption' 
             placeholder="What's on your mind?"
-            onChange={(e) => setCaption(e.target.value)}
-            value={caption} />
+            onChange={(e) => setCaption2(e.target.value)}
+            value={caption2}
+            required />
+
+          <button type='submit'>Submit Post</button>
+        </form>
+      </div>
+
+      <p className='title'>Or..</p>
+
+      <div className='card'>
+        <p className='title'>Create a new photo post</p>
+
+        <form onSubmit={handleSubmit} className='input-container'>
+        <input 
+            type='text' 
+            name='post-caption' 
+            id='postCaption' 
+            placeholder="What's on your mind?"
+            onChange={(e) => setCaption1(e.target.value)}
+            value={caption1}
+            required />
 
           <input 
             type="file" 
             name='addPostPhoto'
             id='addPostPhoto'
             onChange={(e) => setImg(e.target.files[0])}
-            accept='.jpg, .jpeg' />
+            accept='.jpg, .jpeg' 
+            required/>
           <label htmlFor='addPostPhoto' className='post-photo-label'>Add Photo to Post</label>
 
           <button type='submit'>Submit Post</button>
